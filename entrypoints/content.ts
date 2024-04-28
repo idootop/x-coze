0b6b9d4052efee60fc55a11aabc73cfea14f28c9
Del <wjg12341234@163.com>
2024-04-28 11:21:28 +0800

    fix: 适配最新版本 Coze 界面

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -41,43 +41,54 @@ const shouldFreeCozeNow = async () => {
   const url = window.location.href;
   const regex = /space\/(\d+)\/bot\/(\d+)/;
   const isCozeBotPage = url.match(regex);
-  let container = document.querySelector(".sidesheet-container");
-  if (!container) {
+  let container = getContainer();
+  const modelSelector = getModelSelector();
+  if (!container || !modelSelector) {
     return false;
   }
-  const isExpectLayout = container?.children?.length === 3;
+  const isExpectLayout = container?.children?.length === 2;
   const initialized =
-    isExpectLayout && container.children[1].style.position === "fixed";
+    isExpectLayout && modelSelector.style.position === "fixed";
   if (isCozeBotPage && !initialized) {
     return await sendMessage("shouldFreeCoze", {}, "background");
   }
+};
+
+const getContainer = () => {
+  return document.querySelector(".sidesheet-container");
+};
+
+const getModelSelector = () => {
+  return document.querySelector(
+    'button[aria-haspopup="dialog"].semi-button-with-icon'
+  );
 };
 
 const freeCoze = async () => {
   if (!(await shouldFreeCozeNow())) {
     return;
   }
+
   hideElements();
   replaceTitle();
+
   document.documentElement.style.width = "100%";
   document.body.style.width = "100%";
   document.documentElement.style.minWidth = "100%";
   document.body.style.minWidth = "100%";
-  let container = document.querySelector(".sidesheet-container");
-  if (!container) {
-    return false;
-  }
-  container.children[0].style.display = "none";
-  container.children[1].style.position = "fixed";
-  container.children[1].style.zIndex = 1;
-  container.children[1].style.left = "50%";
-  container.children[1].style.height = "auto";
-  container.children[1].style.transform = "translateX(-50%)";
-  container.children[1].style.background = "transparent";
-  container.children[1].children[0].style.height = "52px";
-  container.children[1].children[0].children[1].style.width = "auto";
-  container.children[1].children[0].style.borderBottom = "none";
-  container.children[1].children[1].style.display = "none";
-  container.children[2].style.width = "100vw";
+
+  let container = getContainer();
+  container.children[1].style.width = "100vw";
+  container.children[1].children[0].style.boxShadow =
+    "inset 0px -0.5px 0px rgba(0,0,0,20%)";
+
+  const modelSelector = getModelSelector();
+  modelSelector.style.position = "fixed";
+  modelSelector.style.zIndex = 1;
+  modelSelector.style.left = "50%";
+  modelSelector.style.height = "auto";
+  modelSelector.style.transform = "translateX(-50%)";
+  modelSelector.style.background = "transparent";
+
   return true;
 };
